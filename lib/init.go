a590a7a127add37d4ef9b2bed405e518784e796b
miseyu <miseyu2000@gmail.com>
2018-05-17 11:51:04 +0900

    [add] libを追加

---
+++ lib/init.go
@@ -0,0 +1,12 @@
+package lib
+
+import (
+	"fmt"
+)
+
+// Init initializes github-issue-fetcher settings
+func Init() error {
+	fmt.Print("** github-issue-fetcher Initialization **\n")
+
+	return nil
+}
---
+++ lib/version.go
@@ -0,0 +1,4 @@
+package lib
+
+// Version is the github-issue-fetcher version
+const Version = "0.0.1"
